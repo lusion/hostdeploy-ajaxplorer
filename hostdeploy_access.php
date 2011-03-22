4b0f2772a8d6e9f11c08e1b807395790aa215939
root <root@s1.hostdeploy.com>
2011-03-22 19:20:35 +0000

    Fix time check in hostdeploy access

--- hostdeploy_access.php
+++ hostdeploy_access.php
@@ -6,28 +6,28 @@ defined('AJXP_EXEC') or die( 'Access not allowed');
  * HOSTDEPLOY ACCESS TEST
  ***************************/
 if (isset($_GET['hostdeploy-key'])) {
+	unset($_SESSION['hd_user']);
+
 	$key = $_GET['hostdeploy-key'];
 	$key = base64_decode(strtr($key, '-_', '+/'));
 	list($user, $time, $key) = explode(':', $key, 3);
 
 	// Only allow usage within 30 seconds
-	if (abs($time-time()) > 30) {
-		return;
-	}
-
-	// Hard-coded public key for hostdeploy
-	$public = '-----BEGIN PUBLIC KEY-----
+	if (abs($time-time()) < 30) {
+  	// Hard-coded public key for hostdeploy
+		$public = '-----BEGIN PUBLIC KEY-----
 MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDkW80PWHQXkrf6+LGHU+fiAnCg
 NQ2TUQonJOuVSh+eXi5euUWe8o5nagQxqCO7EmN+NnlG9R4Rmi7YymslvZl04oa5
 OUAO6Ld8JT12TPfAnI+23bBkLb4Idft7qySVFqVWoiNObLC6xaZKlTgQ3NGlmneP
 GHuDRkSSrbl4n0yAZwIDAQAB
 -----END PUBLIC KEY-----';
 
-	$result = openssl_verify("$user:$time", $key, $public);
-	if ($result === 1) {
-		$_SESSION['hd_user'] = $user;
-	}else{
-		throw new Exception("Unexpected result from openssl_verify: ".$result);
+		$result = openssl_verify("$user:$time", $key, $public);
+		if ($result === 1) {
+			$_SESSION['hd_user'] = $user;
+		}else{
+			throw new Exception("Unexpected result from openssl_verify: ".$result);
+		}
 	}
 }
 
